9ba105ea21b3c3b6c315d1c99dee0f20594708fa
PWalis <pwilky1016@gmail.com>
2019-05-31 12:39:09 -0700

    put the ranInt in the __init__ method instead of the args so that it actually makes a new identifier each time

--- acme.py
+++ acme.py
@@ -3,12 +3,12 @@ import unittest
 
 class Product:
     def __init__(self, name, price=10, weight=20, flammability=0.5,
-            identifier=random.randint(1000000, 9999999)):
+            identifier=None):
             self.name = name
             self.price = price 
             self.weight = weight
             self.flammability = flammability
-            self.identifier = identifier
+            self.identifier = random.randint(1000000, 9999999)
 
     def stealability(self):
         '''
